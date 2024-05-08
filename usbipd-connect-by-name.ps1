1cc9a60a4236b0be6665abacbd66aceacd2e4639
hitsmaxft <mfthits@gmail.com>
2024-05-08 23:41:22 +0800

    update script , support search by name

--- usbipd-connect-by-name.ps1
+++ usbipd-connect-by-name.ps1
@@ -1,12 +1,17 @@
 #!/usr/bin/env powershell.exe
 
 # auto gen by chatgpt4
-
-param([string]$vid_pid)
+param ([boolean]$verbose)
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 [Console]::InputEncoding = [System.Text.Encoding]::UTF8
 
+$bootloaderNames = @("STM32  BOOTLOADER", "Maple 003")
 $firstOutput = 1
+
+
+if ($verbose) {
+    Write-Host "search usb device with following names: $bootloaderNames"
+}
 
 while ($true) {
     # STEP0
@@ -22,12 +27,12 @@ while ($true) {
 
     # Write-Host $relevantOutput
     # Check if the VID:PID exists in the list
-    $deviceLine = ($relevantOutput -split "\n") | Where-Object { $_ -match "$vid_pid" }
+    $deviceLine = ($relevantOutput -split "\n") | Where-Object { $_ -match "^[1-9]-[1-9]" } | where { $_ | select-string $bootloaderNames } 
 
     if (-not $deviceLine) {
         # STEP3 - Report error
         if ($FIrstOutput) {
-            Write-Host -NoNewline "device ${vid_pid} not found"
+            Write-Host -NoNewline "no bootloader device not found"
             $firstOutput =  0
         } else {
             Write-Host -NoNewline "."
@@ -35,22 +40,33 @@ while ($true) {
         Start-Sleep -Seconds 1
         continue
     } else {
-            Write-Host "\n"
+            Write-Host ""
+            if ($verbose)  {
+                Write-Host "found device line $deviceLine\n"
+            }
             $firstOutput = 1
     }
 
     # Parsing the variables
     $parts = $deviceLine -split "\s{2,}"
 
-    # debug output
-    # foreach ($part in $parts) {
-    #     Write-Output "["$part"]"
-    # }
+    #debug output
+    if ($verbose)  {
+        Write-Host "found device line '$deviceLine'"
+        foreach ($part in $parts) {
+            Write-Host -NoNewline "["$part"] "
+        }
+        Write-Host ""
+    }
 
     $busId = $parts[0]
     $vidPid = $parts[1]
     $device = $parts[2]
     $state = $parts[-1].TrimStart()
+
+    if ($verbose) {
+        Write-Host "attach device $vidpid into wsl2..."
+    }
 
     # Check the status of the device
     switch ($state) {
@@ -61,7 +77,7 @@ while ($true) {
         }
         default {
             # STEP1 - Device is shared, try to attach
-            Write-Host "state is "$state
+            Write-Host "state is $state"
             try {
                 Write-Host "Connecting"
                 sudo usbipd.exe wsl attach --busid $busId
